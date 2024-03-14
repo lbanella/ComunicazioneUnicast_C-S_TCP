c974125a166ed732fc25f48f2784461aaf628f70
lbanella <banella.lorenzo@gmail.com>
2024-03-14 17:50:51 +0100

    primi sviluppi alla  classe MainServer.java

--- src/main/java/com/mycompany/client_server_application/MainServer.java
+++ src/main/java/com/mycompany/client_server_application/MainServer.java
@@ -8,6 +8,8 @@ package com.mycompany.client_server_application;
 public class MainServer {
 
     public static void main(String[] args) {
-       throw new UnsupportedOperationException("The method is not implemented yet.");
+      Server server=new Server(1789);
+      server.attendi();
+      server.termina();
     }
 }
