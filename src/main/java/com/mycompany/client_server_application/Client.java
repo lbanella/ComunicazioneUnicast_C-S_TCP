ff1cd5469803149cd7742f919a730c76fb0d91f9
lbanella <banella.lorenzo@gmail.com>
2024-03-14 17:50:23 +0100

    primi sviluppi alla  classe Client.java

--- src/main/java/com/mycompany/client_server_application/Client.java
+++ src/main/java/com/mycompany/client_server_application/Client.java
@@ -1,5 +1,11 @@
 package com.mycompany.client_server_application;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Banella Lorenzo
@@ -9,13 +15,28 @@ public class Client {
 
     private String nome;
     private String colore;
+    private Socket socket;
 
     public Client(String nomeDefault, String coloreDefault) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+        this.nome = nomeDefault;
+        this.colore = coloreDefault;
     }
 
     public void connetti(String nomeServer, int portaServer) {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+        System.out.println(" ______________________________\n");
+        System.out.println("      Client "+nome+" in esecuzione\n");
+        System.out.println(" ______________________________\n");
+        try {
+            this.socket = new Socket(nomeServer, portaServer); 
+            //chiudi();
+        } catch(UnknownHostException ex){ 
+            System.err.println("Host sconosciuto!");
+            System.err.println( ex.getMessage());
+        } catch (IOException e){
+            System.err.println("Errore!");
+            System.err.println( e.getMessage());
+        }
+        
     }
 
     public void scrivi() {
@@ -27,7 +48,14 @@ public class Client {
     }
 
     public void chiudi() {
-        throw new UnsupportedOperationException("The method is not implemented yet.");
+            try {
+                // Chiudi il socket solo se è stato inizializzato correttamente
+                this.socket.close();
+                System.out.println("Connessione chiusa.");
+            } catch (IOException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
     }
 
 }
