c5b08b0db6bee7394207aaf5be5b199bcd47aa29
lbanella <banella.lorenzo@gmail.com>
2024-03-18 22:29:24 +0100

    Modifiche alla Classe MainServer metodi : main(String[] args)

---
+++ src/main/java/com/mycompany/client_server_application/Server/MainServer.java
@@ -0,0 +1,18 @@
+package com.mycompany.client_server_application.Server;
+
+
+
+/**
+ *
+ * @author Banella Lorenzo
+ */
+
+public class MainServer {
+
+    public static void main(String[] args) {
+        Server server=new Server(1789);
+        server.comunica();  
+        
+      
+    }
+}
